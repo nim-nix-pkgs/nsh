476543f4c5c7dd542aae17fbf8babb24924d648c
hugosenari <hugosenari@gmail.com>
2022-03-12 04:35:56 -0300

    chore: re index nsh

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''nsh: Nim SHell(cross platform)'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-nsh-master.flake = false;
+  inputs.src-nsh-master.owner = "gmshiba";
+  inputs.src-nsh-master.ref   = "refs/heads/master";
+  inputs.src-nsh-master.repo  = "nish";
+  inputs.src-nsh-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-nsh-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-nsh-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
